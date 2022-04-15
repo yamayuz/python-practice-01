b6222b703c9aab0d7425e72cb1c5dde41103f0e7
yamayuz <yuki@yamayulab.com>
2022-04-16 00:05:09 +0900

    [update] add try out traceback

--- exception_test.py
+++ exception_test.py
@@ -1,8 +1,22 @@
 from myerror_class.myerror import *
+import traceback
 
+# try:
+#     raise MyError('my error happens')
+# except MyError as e:
+#     print(type(e))
+#     print(e)
 
+print(1)
 try:
-    raise MyError('my error happens')
-except MyError as e:
-    print(type(e))
-    print(e)
+    print(2)
+    5 / 0
+    print(3)
+except Exception:
+    print(4)
+    text = traceback.format_exc()
+    print(text)
+    f = open('error.log', 'a')
+    traceback.print_exc(file=f)
+
+print(5)
